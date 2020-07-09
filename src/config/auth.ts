809569b5aac678cee86dd9ec9a3a1f67b25ddb5e
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-07-08 23:40:53 -0300

    First version

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'c9e1bfb635498bf2cfa2281b27185ea5',
+    expiresIn: '1d',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  // eslint-disable-next-line no-console
+  console.log('🏃🏽‍♂️ Server is running!');
+});
